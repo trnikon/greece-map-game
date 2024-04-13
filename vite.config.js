116ab6af84008335b84ba5dea95b4f1baa694b2e
Nikon Trougkakos <nikon.t@expertum.cy>
2024-04-13 16:10:35 +0300

    small changes

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/games/greece-map-game/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
